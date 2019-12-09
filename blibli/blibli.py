5677fddf1a4f892cca9f50cdb1cb156601a009e5
changyuyun <969842624@qq.com>
2019-12-09 16:58:17 +0800

    修改

---
+++ blibli/blibli.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+from utils.config import *
+from selenium import webdriver
+
+class Crack_bb(object):
+    def __init__(self):
+        self.url = URL
+        self.email = EMAIL
+        self.password = PASSWORD
+        self.brower = webdriver.Chrome()
+    def __del__(self):
+        self.brower.close()
+    def crack_login(self):
+        print(1)
+    def open(self):
+        print(1)
+
+
+
+
+if __name__ == '__main__':
+    crack = Crack_bb()
+    crack.crack_login()
